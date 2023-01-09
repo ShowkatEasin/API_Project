15faadfc8e86d44c84e24d575feaa24db967bbdf
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2023-01-09 23:17:08 +0600

    API Routing

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,17 +2,23 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/get', function () use ($router) {
+    return "This is from get";
 });
+$router->post('/post', function () use ($router) {
+    return "This is from post";
+});
+$router->put('/put', function () use ($router) {
+    return "This is from put";
+});
+$router->delete('/delete', function () use ($router) {
+    return "This is from delete";
+});
+
+$router->post('/osman', function () use ($router) {
+    return "This is Showkat Osman";
+});
+
+
+
