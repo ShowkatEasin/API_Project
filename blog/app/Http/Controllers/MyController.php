3d1e397bcb485a07b6f262227a850e7d5c466066
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2023-01-10 19:10:13 +0600

    Ridect

---
+++ blog/app/Http/Controllers/MyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+
+
+class MyController extends Controller{
+
+public function First(){
+    return redirect('/Second');
+
+}
+
+public function Second(){
+    return "I am Second from redirect";
+}
+
+public function Download(){
+    $path = 'demo.txt';
+    return response()->download($path);
+}
+
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,28 +1,10 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
-
-$router->get('/get', function () use ($router) {
-    return "This is from get";
-});
-$router->post('/post', function () use ($router) {
-    return "This is from post";
-});
-$router->put('/put', function () use ($router) {
-    return "This is from put";
-});
-$router->delete('/delete', function () use ($router) {
-    return "This is from delete";
-});
-
-$router->post('/osman', function () use ($router) {
-    return "This is Showkat Osman";
-});
-
-$router->post('/{name}/{age}[/{city}]', function($name, $age, $city=null){
-    return $name.$age.$city ;
-});
 
 
 
+$router->get('/First', 'MyController@First');
+
+$router->get('/Second', 'MyController@Second');
+
+$router->get('/Download', 'MyController@Download');
